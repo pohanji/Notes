6f2aa5271b43084053ca94d6ee9cc0ca76299f91
Jiří Pohančeník <jiri.pohancenik@1pf.cz>
2020-10-22 08:30:43 +0200

    Init commit

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Models
+{
+    [Table("Notes")]
+    class Note
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string NoteBody { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime EditedAt { get; set; }
+    }
+}
