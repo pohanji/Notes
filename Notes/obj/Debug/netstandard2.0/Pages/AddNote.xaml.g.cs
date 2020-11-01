9865f8f1ca3632b0d179b59eb666fb3da4d09e55
Jiří Pohančeník <jiri.pohancenik@1pf.cz>
2020-11-01 20:25:38 +0100

    Už to funguje? Já čekám až to začne fungovat

--- Notes/obj/Debug/netstandard2.0/Pages/AddNote.xaml.g.cs
+++ Notes/obj/Debug/netstandard2.0/Pages/AddNote.xaml.g.cs
@@ -17,8 +17,16 @@ namespace Notes.Pages {
     public partial class AddNote : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry Name;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry Body;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(AddNote));
+            Name = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Name");
+            Body = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Body");
         }
     }
 }
